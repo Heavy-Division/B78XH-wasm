3526e795801af396dc8cfaf3824c1cd34fd13641
김라댜 <kimlada@divisions.cz>
2022-09-14 06:25:33 +0200

    updated SimConnector

--- B78XH-wasm/SimConnectConnector.h
+++ B78XH-wasm/SimConnectConnector.h
@@ -28,19 +28,20 @@ class SimConnectConnector {
 	public:
 		SimConnectConnector() = default;
 		~SimConnectConnector() = default;
-		bool connect(const char* name);
-		void disconnect();
-		void requestDispatchMessages();
-		void processDispatchMessage(SIMCONNECT_RECV* pData, DWORD* cbData);
-		void prepareDataDefinitions();
-		void prepareClientDataDefinitions();
-		void prepareRequests();
-		HRESULT setDataOnSimObject(DATA_DEFINE_ID DefineID,
-		                           SIMCONNECT_OBJECT_ID ObjectID,
-		                           SIMCONNECT_DATA_SET_FLAG Flags,
-		                           DWORD ArrayCount,
-		                           DWORD cbUnitSize,
-		                           void* pDataSet);
+		auto connect(const char* name) -> bool;
+		auto disconnect() -> void;
+		auto getHandle() -> HANDLE;
+		auto requestDispatchMessages() -> void;
+		auto processDispatchMessage(SIMCONNECT_RECV* pData, DWORD* cbData) -> void;
+		auto prepareDataDefinitions() -> void;
+		auto prepareClientDataDefinitions() -> void;
+		auto prepareRequests() -> void;
+		auto setDataOnSimObject(DATA_DEFINE_ID DefineID,
+		                        SIMCONNECT_OBJECT_ID ObjectID,
+		                        SIMCONNECT_DATA_SET_FLAG Flags,
+		                        DWORD ArrayCount,
+		                        DWORD cbUnitSize,
+		                        void* pDataSet) -> HRESULT;
 };
 
 inline SimConnectConnector connector;
